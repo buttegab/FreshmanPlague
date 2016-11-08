ed357801d5758cc8b3e4feed2c671a916ed4a2cd
Matt Ruehle <matthew.t.ruehle@gmail.com>
2016-11-08 11:43:17 -0500

    Draft of network generation, and creation of code folder

---
+++ code/network_generator.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+Modified Barabasi-Albert Network Generation, for disease spread simulation.
+Complexity Science F2016, Olin College
+"""
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Student(object):
+	""" 
+	Base student; used as node in network. 
+	"""
+
+	def __init__(self, immune_strength=1, tolerance=5):
+		self.immune_strength = immune_strength
+		self.tolerance = tolerance #these do nothing right now.
+
+
+def create_student_ba_graph(size=90, new_edges_per_node=3):
+	""" 
+	Creates a graph of [size] Student nodes, using weighted connections.
+	Eventually, efficiency might be a concern here. Should be fine
+	for n=90, though.
+	TODO: vary the edges new nodes introduce.
+	TODO: roommates, relationships, etc - "colored" edges.
+	"""
+	G = nx.Graph()
+	if size <= 0:
+		return G
+	first_student = Student()
+	G.add_node(first_student)
+	students_list = []
+	students_list.append([first_student,0])
+	for i in range(size):
+		new_student = Student()
+		G.add_node(new_student)
+
+		all_students = range(len(students_list))
+		probs_base = [k[1]+1 for k in students_list]
+		probs = np.linalg.norm(probs_base)
+		print "ahere"
+		number_of_friends = min(len(students_list), new_edges_per_node)
+		print "bhere"
+		print probs
+		print all_students
+		print number_of_friends
+		print "---"
+		new_friends = np.random.choice(all_students, size=number_of_friends, replace=False, p=probs)
+		print "here"
+		for friend in new_friends:
+			students_list[friend][1] += 1
+			G.add_edge(new_student, students_list[friend][0])
+		students_list.append([new_student, nx.degree(G, new_student)])
+	return G
+
+
+
+if __name__=="__main__":
+	test_graph = create_student_ba_graph()
+	nx.draw(test_graph)
+	plt.show()
+
+
+
